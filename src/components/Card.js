5603fee22a971923fb4ff7268e64b4792969edb9
petrinio <facundopetri@gmail.com>
2021-02-18 12:35:19 -0300

    cambio a hooks

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,6 @@
 const Card = (props) => {
   return (
-    <div className="card" id="lol" onClick={() => props.handleClick(props.clicked, props.id)}>
+    <div className="card" onClick={() => props.handleClick(props.clicked, props.id)}>
       <img src={props.url} alt="" />
       <p style={{ fontWeight: "bold" }}>{props.name}</p>
       <p>{props.job}</p>
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useMemo, Component } from "react";
+import { useState, useEffect } from "react";
 import Score from "./Score";
 import Card from "./Card";
 import charactersJson from "../characters.json";
@@ -7,101 +7,73 @@ function shuffle(array) {
   return array.sort(() => Math.random() - 0.5);
 }
 
-class Game extends Component {
-  constructor() {
-    super();
-    this.state = {
-      score: 0,
-      bestScore: 0,
-      characters: [],
-    };
-  }
+const Game = () => {
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
 
-  // const [score, setScore] = useState(0);
-  // const [bestScore, setBestScore] = useState(0);
+  const [characters, setCharacters] = useState([]);
 
-  // const [characters, setCharacters] = useState(charactersJson);
+  useEffect(() => {
+    setCharacters(shuffle(charactersJson));
+  }, [score]);
 
   // useEffect(() => {
-  //   shuffle(characters);
-  // });
+  //   if (score > bestScore) {
+  //     setBestScore(score);
+  //     setScore(0);
+  //   }
+  // }, [score, bestScore])
 
-  // const shuffleCards = useMemo(() => {
-  //   return shuffle(characters)
-  // }, [])
-
-  // const handleClick = (id, event) => {
-  //   console.log(characters);
-  //   setCharacters((prevCharacters) => {
-  //     characters.map(item => {
-  //       if(item.id === id) {
-  //         item.clicked = true
-  //       }
-  //       return item
-  //     })
-  //   });
-  // };
-  
-  componentDidMount() {
-    this.setState({ characters: shuffle(charactersJson) });
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.score !== prevState.score)
-      this.setState({ characters: shuffle(this.state.characters) });
-  }
-
-  handleClick(isClicked, id) {
+  function handleClick(isClicked, id) {
     if (!isClicked) {
-      this.setState((prevState) => {
-        return {
-          characters: this.state.characters.map((item) => {
-            if (item.id === id) {
-              item.clicked = true;
-            }
-            return item;
-          }),
-          score: prevState.score + 1,
-        };
-      });
+      setCharacters(
+        characters.map((item) => {
+          if (item.id === id) {
+            item.clicked = true;
+          }
+          return item;
+        })
+      );
+
+      setScore(score + 1);
     } else {
-      this.newGame();
+      newGame();
     }
   }
 
-  newGame() {
+  function newGame() {
     console.log("perdiste");
-    if (this.state.score > this.state.bestScore)
-      this.setState({ bestScore: this.state.score, score: 0 });
-    this.setState({
-      characters: this.state.characters.map((item) => {
+    if (score > bestScore) {
+      setBestScore(score);
+      setScore(0);
+    }
+    setCharacters(
+      characters.map((item) => {
         if (item.clicked) item.clicked = false;
         return item;
-      }),
-    });
-  }
-
-  render() {
-    const cardCharacters = this.state.characters.map((item, idArray) => {
-      return (
-        <Card
-          key={idArray}
-          id={item.id}
-          url={item.image}
-          name={item.name}
-          job={item.occupation}
-          clicked={item.clicked}
-          handleClick={this.handleClick.bind(this)}
-        />
-      );
-    });
-    return (
-      <div>
-        <Score score={this.state.score} bestScore={this.state.bestScore} />
-        <div className="card-container">{cardCharacters}</div>
-      </div>
+      })
     );
   }
-}
+
+  const cardCharacters = characters.map((item, idArray) => {
+    return (
+      <Card
+        key={idArray}
+        id={item.id}
+        url={item.image}
+        name={item.name}
+        job={item.occupation}
+        clicked={item.clicked}
+        handleClick={handleClick}
+      />
+    );
+  });
+  return (
+    <div>
+      <Score score={score} bestScore={bestScore} />
+      <div className="card-container">{cardCharacters}</div>
+    </div>
+  );
+};
 
 export default Game;
